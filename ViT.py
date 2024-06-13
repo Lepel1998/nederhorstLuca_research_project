1f50a37e6e7937a94a9b1a0d25c3cedc75ea3aef
--global <--global>
2024-06-13 10:33:26 +0200

    ViT.py cleaned and ready for take off

--- ViT.py
+++ ViT.py
@@ -1,35 +1,38 @@
-import os
+import time
+import numpy as np
 import torch
-import requests
-from PIL import Image
 from transformers import ViTFeatureExtractor, ViTModel
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 from torch.utils.data import DataLoader
-import numpy
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Load pre-trained ViT model and feature extractor
+sns.set_style('darkgrid')
+
+
+# load pre-trained ViT model and feature extractor
 feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k', do_rescale=False)
 model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-
 print('pretrained model retrieved')
 
-# Load your dataset
-dataset_path = 'processed_dataset'  # Update with your dataset path
+# load dataset
+dataset_path = 'dataset'
 dataset = ImageFolder(root=dataset_path, transform=transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ]))
 
-# Define DataLoader
+# define DataLoader
 batch_size = 8
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-# Extract features from images
+# extract features from images
 features = []
 labels = []
 
@@ -40,52 +43,103 @@ for images, labels_batch in dataloader:
     labels.extend(labels_batch)
 print('all features extracted through pretrained model')
 
-# Concatenate features and convert labels to tensor
+# concatenate features and convert labels to tensor
 features = torch.cat(features, dim=0)
 labels = torch.tensor(labels)
-(print('1'))
 
-# Split data into train and test sets
-train_features, test_features = features[:int(0.8 * len(features))], features[int(0.8 * len(features)):]
-train_labels, test_labels = labels[:int(0.8 * len(labels))], labels[int(0.8 * len(labels)):]
+# split data into train (80%) and test (20%) sets randomly
+train_features, test_features, train_labels, test_labels = train_test_split(features, 
+                                                                            labels, 
+                                                                            test_size=0.2, 
+                                                                            random_state=2, 
+                                                                            stratify=labels)
+print(train_labels, test_labels)
 
-# Convert tensors to NumPy arrays
+# convert tensors to NumPy arrays, because Scikit-Learn  classifiers require inputs in the form of numpy arrays
 train_features = train_features.detach().cpu().numpy()
 test_features = test_features.detach().cpu().numpy()
 train_labels = train_labels.detach().cpu().numpy()
 test_labels = test_labels.detach().cpu().numpy()
+all_labels = dataset.classes
 
-print('2')
-
-
-# Train and predict SVM classifier on features extracted from ViT
+# train SVM classifier and predict label on features extracted from ViT
 svm_classifier = SVC()
+
+start_time_nb = time.time()
 svm_classifier.fit(train_features, train_labels)
+end_time_nb = time.time()
+train_time_nb = end_time_nb - start_time_nb
+print(f'Training time SVM: {train_time_nb}')
+
 svm_predictions = svm_classifier.predict(test_features)
 svm_accuracy = accuracy_score(test_labels, svm_predictions)
 print(f'SVM Accuracy: {svm_accuracy}')
 
-print('3')
+report_svm = classification_report(test_labels, svm_predictions, labels=range(len(all_labels)), target_names=all_labels)
+print('\nClassification Report SVM:')
+print(report_svm)
 
+confusion_matrix_svm = confusion_matrix(test_labels, svm_predictions, labels=range(len(all_labels)))
+print('Confusion Matrix SVM:')
+plt.figure(figsize=(10,7))
+sns.heatmap(confusion_matrix_svm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix CNN')
+plt.show()
 
-# Train and predict KNN classifier on features extracted from ViT
+# train KNN classifier and predict label on features extracted from ViT
 knn_classifier = KNeighborsClassifier()
+
+start_time_nb = time.time()
 knn_classifier.fit(train_features, train_labels)
+end_time_nb = time.time()
+train_time_nb = end_time_nb - start_time_nb
+print(f'Training time KNN: {train_time_nb}')
+
 knn_predictions = knn_classifier.predict(test_features)
 knn_accuracy = accuracy_score(test_labels, knn_predictions)
 print(f'KNN Accuracy: {knn_accuracy}')
 
-print('4')
+report_knn = classification_report(test_labels, knn_predictions, labels=range(len(all_labels)), target_names=all_labels)
+print('\nClassification Report KNN:')
+print(report_knn)
 
+confusion_matrix_knn = confusion_matrix(test_labels, knn_predictions, labels=range(len(all_labels)))
+print('Confusion Matrix KNN:')
+plt.figure(figsize=(10,7))
+sns.heatmap(confusion_matrix_knn, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix KNN')
+plt.show()
 
-# Train and predict NB classifier on features extracted from ViT
+# train NB classifier and predict label on features extracted from ViT
 nb_classifier = GaussianNB()
+
+start_time_nb = time.time()
 nb_classifier.fit(train_features, train_labels)
+end_time_nb = time.time()
+train_time_nb = end_time_nb - start_time_nb
+print(f'Training time NB: {train_time_nb}')
+
 nb_predictions = nb_classifier.predict(test_features)
 nb_accuracy = accuracy_score(test_labels, nb_predictions)
 print(f'NB Accuracy: {nb_accuracy}')
 
-print('5')
+report_nb = classification_report(test_labels, nb_predictions, labels=range(len(all_labels)), target_names=all_labels)
+print('\nClassification Report NB:')
+print(report_nb)
+
+confusion_matrix_nb = confusion_matrix(test_labels, nb_predictions, labels=range(len(all_labels)))
+print('Confusion Matrix NB:')
+plt.figure(figsize=(10,7))
+sns.heatmap(confusion_matrix_nb, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix NB')
+plt.show()
+
 
 
 
--- pretrained_cnn.py
+++ pretrained_cnn.py
@@ -11,36 +11,50 @@ https://www.youtube.com/watch?v=fCtMf6qHtdk
 
 """
 import torch
+from tensorflow.python.keras.models import load_model
+import os
+import time
 import torch.nn as nn
+from torchvision import models
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms, models
+import seaborn as sns
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from torch.utils.data import DataLoader, random_split
 from pillow_heif import register_heif_opener
 register_heif_opener()
+sns.set_style('darkgrid')
 
 # load dataset
 dataset_directory = 'processed_dataset'
 
 # do augmentation and preprocessing here to skip slow main.py function
-augmentation = transforms.Compose([
+preprocessing = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.RandomRotation(30),
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(), # converts image to tensor
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])    # normalizes tensor using mean and standard deviation values from ImageNet dataset
 ])
 
-# load dataset and perform augmentation
-dataset = datasets.ImageFolder(root = dataset_directory, transform=augmentation)
+# load dataset 
+dataset = datasets.ImageFolder(root = dataset_directory, transform=preprocessing)
+
+# check which label referres to what species
+class_labels = dataset.classes
+label_to_species = {}
+for label in range(len(class_labels)):
+    label_to_species[label] = class_labels[label]
+print("Label to species:")
+print(label_to_species)
+
+# divide dataset into training and test set
 train_size = int(0.8*len(dataset))
-validation_size = len(dataset) - train_size
-train_dataset, validation_dataset = random_split(dataset, [train_size, validation_size])
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 # dataloader to load data into differnet batches
 train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-validation_dataloader = DataLoader(validation_dataset, batch_size = 32, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size = 32, shuffle=False)
 
 # load the EfficientNetB0 model retrieved from torch (CNN with transfer learning)
 #  "https://download.pytorch.org/models/efficientnet_b0_rwightman-7f5810bc.pth"
@@ -52,7 +66,6 @@ for parameter in model.parameters():
 
 # Add custom layers on top of base model, this is also the transfer leanring part
 number_features = model.classifier[1].in_features
-
 model.classifier = nn.Sequential(
     nn.Linear(number_features, 1024),
     nn.ReLU(),
@@ -68,9 +81,10 @@ optimizer = optim.Adam(model.classifier.parameters(), lr=1e-4)
 # save the history of the training model 
 train_losses = []
 train_accuracies = []
+epoch_amount = 10
 
 # training loop
-epoch_amount = 10
+start_time_cnn = time.time()
 for epoch in range(epoch_amount):
     model.train()
     running_loss = 0.0
@@ -78,7 +92,6 @@ for epoch in range(epoch_amount):
 
     for inputs, labels in train_dataloader:
         optimizer.zero_grad()
-
         outputs = model(inputs)
         loss = loss_criterion(outputs, labels)
         loss.backward()
@@ -107,7 +120,7 @@ for param in model.features[-20:].parameters():
 
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-5)
 
-fine_tune_epochs = 10
+fine_tune_epochs = 2
 total_epochs = epoch_amount + fine_tune_epochs
 for epoch in range(epoch_amount, total_epochs):
     model.train()
@@ -135,6 +148,9 @@ for epoch in range(epoch_amount, total_epochs):
     train_losses.append(epoch_loss)
     train_accuracies.append(epoch_acc)
 
+end_time_cnn = time.time()
+train_time_cnn = end_time_cnn - start_time_cnn
+print(f'Training time CNN: {train_time_cnn}')
 
 # evaluate the model
 model.eval()
@@ -142,11 +158,11 @@ validation_loss = 0.0
 validation_corrects = 0.0
 
 # save the history of the training model
-validation_losses = []
-validation_accuracies = []
+true_labels = []
+predicted_labels = []
 
 with torch.no_grad():
-    for inputs, labels in validation_dataloader:
+    for inputs, labels in test_dataloader:
         outputs = model(inputs)
         loss = loss_criterion(outputs, labels)
 
@@ -154,22 +170,44 @@ with torch.no_grad():
         validation_loss = validation_loss + loss.item() * inputs.size(0)
         validation_corrects = validation_corrects + torch.sum(predictions == labels.data)
 
-        validation_losses.append(validation_loss)
-        validation_accuracies.append(validation_corrects)
-    
-validation_loss = validation_loss / validation_size
-validation_accuracy = validation_corrects.double() / validation_size
+        true_labels.extend(labels.cpu().numpy().astype(int))
+        predicted_labels.extend(predictions.cpu().numpy().astype(int))
+        #predicted_labels.append(validation_corrects)
+       
+print(f'True labels: {true_labels[:5]}')
+print(f'Predicted labels: {predicted_labels[:5]}')   
+validation_loss = validation_loss / test_size
+validation_accuracy = validation_corrects.double() / test_size
 
 print(f'Validation loss: {validation_loss:.4f}, Validation Accuracy: {validation_accuracy:.4f}')
 
-
 # plot accuracy of model training in plot
 plt.plot(train_losses, 'r', label='train_loss')
-plt.plot(validation_losses, 'm', label='validation_loss')
+plt.plot(true_labels, 'm', label='validation_loss')
 plt.plot(train_accuracies, 'b', label='train_accuracy')
-plt.plot(validation_accuracies, 'g', label='validation accuracy')
+plt.plot(predicted_labels, 'g', label='validation accuracy')
 plt.suptitle('Loss and Accuracy', fontsize=22)
 plt.legend(loc='right')
 plt.ylabel('Value')
 plt.ylim(0, 1)
 plt.show()
+
+# print 
+report = classification_report(true_labels, predicted_labels)
+print('\nClassification Report CNN:')
+print(report)
+
+# confusion matrix
+confusion_matrix_pretrained_cnn = confusion_matrix(true_labels, predicted_labels)
+plt.figure(figsize=(10,7))
+sns.heatmap(confusion_matrix_pretrained_cnn, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix CNN')
+plt.show()
+
+
+model_path = 'InClasPreTrainedCNN_Model.h5'
+torch.save(model.state_dict(), model_path)
+print(f'Model saved to {model_path}')
+
